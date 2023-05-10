4818795daee9866ac8cc84ccada217b046f10388
Leonid Tolstov <leonski@mail.ru>
2023-05-10 14:20:04 +0300

    phone book const

--- menu.py
+++ menu.py
@@ -8,16 +8,27 @@ def start_menu():
                         '\n4. Добавить контакт'
                         '\n5 Выйти'
                         '\n--> '))
-        if change == 1:
-            data_worker.get_phone_book()
-        elif change == 2:
-            data_worker.search_phone_book()
-        elif change == 3:
-            data_worker.del_contact()
-        elif change == 4:
-            data_worker.add_contact()
-        else:
-            break
+        # if change == 1:
+        #     data_worker.get_phone_book()
+        # elif change == 2:
+        #     data_worker.search_phone_book()
+        # elif change == 3:
+        #     data_worker.del_contact()
+        # elif change == 4:
+        #     data_worker.add_contact()
+        # else:
+        #     break
+        match change:
+            case 1:
+                data_worker.get_phone_book()
+            case 2:
+                data_worker.search_phone_book()
+            case 3:
+                data_worker.del_contact()
+            case 4:
+                data_worker.add_contact()
+            case _:
+                break
 
 
 
