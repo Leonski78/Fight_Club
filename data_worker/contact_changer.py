93d6113f2f6fd18d4d394bcc148cba351a8128aa
Leonid Tolstov <leonski@mail.ru>
2023-05-12 12:34:14 +0300

    commit \n'

--- data_worker/contact_changer.py
+++ data_worker/contact_changer.py
@@ -4,7 +4,7 @@ def del_contact():
     with open(FILE_PATH, 'r', encoding='utf8') as Phone_book:
         all_phone_book = Phone_book.readlines()
 
-    del_contact_data = input('Введите данные для поиска: ')
+    del_contact_data = input('Введите данные для удаления: ')
     result = [line for line in all_phone_book if del_contact_data not in line]
 
     with open(FILE_PATH, 'w', encoding='utf8') as Phone_book:
@@ -26,10 +26,13 @@ def change_contact():
         change_phone_book = Phone_book.readlines()
     change_contact_data = input('Введите данные для изменения: ')
 
-    for i in range (len(change_phone_book)):
+    for i in range(len(change_phone_book)):
         if change_contact_data in change_phone_book[i]:
-            change_phone_book [i] = input('Введите новые данные: ')
-        break    
+            if i != len(change_phone_book) - 1:
+                change_phone_book[i] = input('Введите новые данные: ') + '\n'
+            else:
+                change_phone_book[i] = input('Введите новые данные: ')
+        break
 
     with open(FILE_PATH, 'w', encoding='utf8') as Phone_book:
         for line in change_phone_book:
